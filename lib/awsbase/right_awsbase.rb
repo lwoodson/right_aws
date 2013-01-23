ad00e8c09d767635b81238a538c799b7105ea8e2
Lance Woodson <lance.woodson@peopleadmin.com>
2013-01-23 04:07:52 -0600

    Longer connection timeout to allow me to work at home

--- lib/awsbase/right_awsbase.rb
+++ lib/awsbase/right_awsbase.rb
@@ -424,7 +424,10 @@ module RightAws
       end
       connection = (connection_storage[server_url] ||= {})
       connection[:last_used_at] = Time.now
-      connection[:connection] ||= Rightscale::HttpConnection.new(:exception => RightAws::AwsError, :logger => @logger)
+      connection[:connection] ||= Rightscale::HttpConnection.new(:exception => RightAws::AwsError, 
+                                                                 :logger => @logger,
+                                                                 :http_connection_open_timeout => 15,
+                                                                 :http_connection_retry_count => 5)
     end
 
     #----------------------------
